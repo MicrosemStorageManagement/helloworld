cac7d53ab71096bfe7aed8e16dff3e95e446f232
microsemistoragemngt <sona.nagarajan@microsemi.com>
2016-11-25 15:06:33 +0530

    Add files via upload
    initial commit

---
+++ HelloWorld/src/org/microsemi/sample/HelloWorldSample.java
@@ -0,0 +1,22 @@
+package org.microsemi.sample;
+
+/**
+ * This is a sample class for trial purpose
+ * Print Hello to the world
+ */
+public class HelloWorldSample
+{
+   public HelloWorldSample()
+   {
+      
+   }
+   /**
+    * @param args
+    */
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!!!");
+
+   }
+
+}
